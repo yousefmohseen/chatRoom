8aa6971baa115e89b9b6d1d924cd88f8ed580061
yousefmohseen <abuturaab12345@gmail.com>
2025-11-26 15:09:24 +0300

    Site upgrade and global operation v1.0

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-const SERVER = import.meta.env.VITE_SERVER_URL || "https://chatroom-backend-production-6898.up.railway.app/";
+const SERVER = import.meta.env.VITE_SERVER_URL || "https://chatroom-backend-production-6898.up.railway.app";
 
 export const socket: Socket = io(SERVER, {
   autoConnect: false
